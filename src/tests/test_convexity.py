5a723b5914a8716387ea27ff78922192563809f9
Ashvin Ranjan <ranjanashvin@gmail.com>
2025-07-10 16:09:47 -0700

    More remote debugging

--- src/tests/test_convexity.py
+++ src/tests/test_convexity.py
@@ -100,7 +100,8 @@ class TestConvexity:
             - self.sim_space.encoder_convexity(np.ones((9, 1)) / 9, np.array([1]))
             < IB_EPSILON
         )
-        print(simple_lang.reconstructed_meanings)
+        print(simple_lang.expressions_prior)
+        print(np.array_equal(np.array([[0.465, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.465]]).T, simple_lang.reconstructed_meanings))
         assert (
             self.sim_space.language_convexity(simple_lang, referents=True, steps=1000)
             - self.sim_space.encoder_convexity(
